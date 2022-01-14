27f519305d112585899b1dfb856a32ebde1175c8
prakharsingh2818 <prakhar.singh@knoldus.com>
2022-01-14 22:49:55 +0530

    Added comments

--- src/main/scala/edu/knoldus/TailRecursiveFactorial.scala
+++ src/main/scala/edu/knoldus/TailRecursiveFactorial.scala
@@ -6,8 +6,8 @@ class TailRecursiveFactorial {
   def factorial(number: Int): BigInt = {
     @tailrec
     def calculateFactorial(number: Int, accumulator: BigInt = 1): BigInt = {
-      if (number <= 1) accumulator
-      else calculateFactorial(number - 1, number * accumulator)
+      if (number <= 1) accumulator //Accumulator is returned as final result
+      else calculateFactorial(number - 1, number * accumulator) //Result is stored in the accumulator
     }
     calculateFactorial(number)
   }
--- src/test/scala/edu/knoldus/LogTest.scala
+++ src/test/scala/edu/knoldus/LogTest.scala
@@ -1,12 +1,9 @@
 package edu.knoldus
 
 import org.scalatest.flatspec.AnyFlatSpec
-import org.slf4s.{Logger, LoggerFactory}
 
 class LogTest extends AnyFlatSpec {
   val log = new Log()
-  val testLogger: Logger = LoggerFactory.getLogger(classOf[Log])
-
 
   "Log message for given message and level" should "be" in {
     val level = "one"
@@ -15,7 +12,6 @@ class LogTest extends AnyFlatSpec {
   }
 
   "Log message for given message only" should "be" in {
-    val level = "INFO"
     val message = "check"
     assertResult("[INFO]:check")(log.logMessage(message))
   }
