ec131f738d9ea086793c7a0d1aa6404f6d1b229d
prakharsingh2818 <prakhar.singh@knoldus.com>
2022-01-15 23:10:56 +0530

    Cosmetic changes

--- src/main/scala/edu/knoldus/Log.scala
+++ src/main/scala/edu/knoldus/Log.scala
@@ -1,12 +1,7 @@
 package edu.knoldus
 
-import org.slf4s.{Logger, LoggerFactory}
-
-
 class Log {
   def logMessage(message: String, level: String = "INFO"): String = {
-    val logger: Logger = LoggerFactory.getLogger(classOf[Log])
-    logger.info(s"[$level]:$message")
     s"[$level]:$message"
   }
 }
